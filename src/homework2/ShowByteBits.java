5b6fb2427b2da6d985f78cf5e2f19ca5bbb698ac
Ah Yeong Lee <leeay0219@gmail.com>
2018-03-28 19:13:03 +0900

    add ShowByteBits.java

---
+++ src/homework2/ShowByteBits.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ShowByteBits {
+    public static void main(String...args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Input number");
+        byte bv = sc.nextByte();
+        System.out.printf("%d", (bv & 0x80) >> 7);
+        System.out.printf("%d", (bv & 0x40) >> 6);
+        System.out.printf("%d", (bv & 0x20) >> 5);
+        System.out.printf("%d", (bv & 0x10) >> 4);
+        System.out.printf("%d", (bv & 0x08) >> 3);
+        System.out.printf("%d", (bv & 0x04) >> 2);
+        System.out.printf("%d", (bv & 0x02) >> 1);
+        System.out.printf("%d\n", (bv & 0x01));
+    }
+
+}
