e6750be6ae93b20ab0b5d9a9c6ae37f1ddb1984c
leeay0219 <35085704+leeay0219@users.noreply.github.com>
2018-04-09 18:24:54 +0900

    Update ShowByteBits.java

--- src/homework2/ShowByteBits.java
+++ src/homework2/ShowByteBits.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class ShowByteBits {
     public static void main(String...args) {
         Scanner sc = new Scanner (System.in);
-        System.out.println("Input number");
         byte bv = sc.nextByte();
         System.out.printf("%d", (bv & 0x80) >> 7);
         System.out.printf("%d", (bv & 0x40) >> 6);
